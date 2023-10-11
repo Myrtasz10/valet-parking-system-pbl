8c334e4b8ea0b3f06d2008e519caffd82efc60ce
Myrtasz10 <myrtasz10@gmail.com>
2023-10-11 20:49:21 +0200

    działające animacje + siatka

--- app_window.py
+++ app_window.py
@@ -24,37 +24,38 @@ class Table(QWidget):
         self.button = QPushButton('Start', clicked=self.animation)
         self.layout.addWidget(self.button)
 
+        # Create a QGraphicsView to display the grid
+        self.view = QGraphicsView(self)
+        self.view.setGeometry(10, 10, 317, 325)  # Adjust the geometry as needed
+
+        # Create a QGraphicsScene and set it in the view
+        self.scene = QGraphicsScene(self)
+        self.view.setScene(self.scene)
+
+        # Add a grid of items to the scene
+        self.addGrid()
         self.frame = QFrame(self)
         self.frame.setStyleSheet('background-color: red;')
         self.frame.setFrameStyle(QFrame.Panel | QFrame.Raised)
-        self.frame.move(10, 10)
-        self.frame.resize(100, 100)
+        self.frame.move(124, 128)
+        self.frame.resize(90, 90)
         
-        table_widget = QTableWidget()
-
-        # Set the number of rows and columns
-        table_widget.setRowCount(3)
-        table_widget.setColumnCount(3)
-        
+    def addGrid(self):
         cell_size = 100
-        
-        for i in range(3):
-            table_widget.setColumnWidth(i, cell_size)
-            table_widget.setRowHeight(i, cell_size)
+        num_rows = 3
+        num_cols = 3
 
-        # Populate the table with items (rectangular cells)
-        for i in range(3):
-            for j in range(3):
-                item = QTableWidgetItem()
-                table_widget.setItem(i, j, item)
-
-        self.layout.addWidget(table_widget)
-        self.window.setLayout(self.layout)  # Set the layout for the main window
-        self.window.setWindowTitle("3x3 Table Example")
+        for row in range(num_rows):
+            for col in range(num_cols):
+                x = col * cell_size
+                y = row * cell_size
+                rect_item = QGraphicsRectItem(x, y, cell_size, cell_size)
+                rect_item.setPen(QColor(Qt.black))  # Grid lines' color
+                self.scene.addItem(rect_item)
 
     def animation(self):
         self.animation = QPropertyAnimation(self.frame, b'geometry')
-        self.animation.setDuration(10000) # mm seconds
-        self.animation.setStartValue(QRect(10, self.frame.y(), 100, 100))
-        self.animation.setEndValue(QRect(10, self.frame.y(), 200, 200))
+        self.animation.setDuration(1000) # mm seconds
+        self.animation.setStartValue(QRect(124, 128, 90, 90))
+        self.animation.setEndValue(QRect(224, 128, 90, 90))
         self.animation.start()
