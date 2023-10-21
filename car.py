4a014dce6322f6f23deffe317a4814faf623114f
krzsztfwtk <krzsztfwtk>
2023-10-21 09:51:26 +0200

    contex menu for adding and moving car

--- car.py
+++ car.py
@@ -1,5 +1,5 @@
-from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QGraphicsRectItem
+from PyQt5.QtGui import QColor, QContextMenuEvent
+from PyQt5.QtWidgets import QGraphicsRectItem, QMenu, QAction
 from PyQt5.QtCore import QVariantAnimation, QPointF
 
 class Car(QGraphicsRectItem):
@@ -7,16 +7,39 @@ class Car(QGraphicsRectItem):
         super().__init__(x, y, width, height)
         self.setBrush(QColor('red'))
 
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu()
+        moveUp = QAction('Move Up')
+        moveDown = QAction('Move Down')
+        moveLeft = QAction('Move Left')
+        moveRight = QAction('Move Right')
+
+        moveUp.triggered.connect(self.move_up)
+        moveDown.triggered.connect(self.move_down)
+        moveLeft.triggered.connect(self.move_left)
+        moveRight.triggered.connect(self.move_right)
+
+        contextMenu.addAction(moveUp)
+        contextMenu.addAction(moveDown)
+        contextMenu.addAction(moveLeft)
+        contextMenu.addAction(moveRight)
+        contextMenu.exec_(event.screenPos())
+
+        
     def animate(self, start_space, end_space, cell_size, speed, distance):
-        # Calculate time
         time = float(distance) / float(speed)
         time *= 1000  # Convert to milliseconds
         time = int(time)
 
-        start_x = (start_space[1] * cell_size) + (cell_size - self.rect().width()) / 2
-        start_y = (start_space[0] * cell_size) + (cell_size - self.rect().height()) / 2
-        end_x = (end_space[1] * cell_size) + (cell_size - self.rect().width()) / 2
-        end_y = (end_space[0] * cell_size) + (cell_size - self.rect().height()) / 2
+        # start_x = (start_space[1] * cell_size) + (cell_size - self.rect().width()) / 2
+        # start_y = (start_space[0] * cell_size) + (cell_size - self.rect().height()) / 2
+        # end_x = (end_space[1] * cell_size) + (cell_size - self.rect().width()) / 2
+        # end_y = (end_space[0] * cell_size) + (cell_size - self.rect().height()) / 2
+
+        start_x = (start_space[1] * cell_size)
+        start_y = (start_space[0] * cell_size)
+        end_x = (end_space[1] * cell_size)
+        end_y = (end_space[0] * cell_size)
 
         self.animation = QVariantAnimation()
         self.animation.setDuration(time)
@@ -24,6 +47,7 @@ class Car(QGraphicsRectItem):
         self.animation.setEndValue(QPointF(end_x, end_y))
         self.animation.valueChanged.connect(self.setPos)
         self.animation.start()
+
 
 
     def move_up(self):
@@ -37,4 +61,3 @@ class Car(QGraphicsRectItem):
 
     def move_right(self):
         self.animate((self.y() // 100, self.x() // 100), (self.y() // 100, (self.x() + 100) // 100), 100, 1, 1)
-
--- main.py
+++ main.py
@@ -12,12 +12,6 @@ if __name__ == '__main__':
     parking_lot = ParkingLot(parser)
     parking_lot.show()
 
-    # Example usage of the animate function
-    start_space = (0, 0)
-    end_space = (0, 1)
-    cell_size = 100  # This should match the size used in addGrid
-
-    parking_lot.car.animate(start_space, end_space, cell_size, parser.speed, parser.parking_spot_width)
 
     try:
         sys.exit(app.exec_())
